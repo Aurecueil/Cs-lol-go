ed1e3acea6b4fdcf9e5e82986ede3c0fab68093e
Aurecueil <leewampl@gmail.com>
2026-07-31 06:14:49 -0700

    config log trace

--- ModLoader/CSLolHostManager.cs
+++ ModLoader/CSLolHostManager.cs
@@ -148,6 +148,7 @@ namespace ModManager
 
                             // Commands sent sequentially to LTK Host IPC
                             await writer.WriteLineAsync("config loglevel 16");
+                            await writer.WriteLineAsync("config log trace");
                             await writer.WriteLineAsync($"config flags {configFlags}");
                             await writer.WriteLineAsync($"config prefix {overlayPrefixPath}");
                             await writer.WriteLineAsync("start scan");
